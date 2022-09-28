76537ee5125d8631027936981e2d06793e50c287
Bog3008 <106614305+Bog3008@users.noreply.github.com>
2022-09-28 12:24:20 +0300

    Update HW3.cpp

--- Toporkov Bogdan 221/HW3.cpp
+++ Toporkov Bogdan 221/HW3.cpp
@@ -1,35 +1,49 @@
+#include <iostream>
+using namespace std;
 template <class T>
-        struct u_ptr{
-            explicit u_ptr(T& a): ptr(new T){
-                *ptr = a;
-            }
-            ~u_ptr(){
-                if(!ptr) return;
-                delete ptr;
-                ptr = nullptr;
-            }
-            u_ptr<T>& operator = (u_ptr<T> & a){
-                if(!a.ptr) throw 1;
-                if(this == &a) return this;
-                delete ptr;
-                ptr = a.ptr;
-                a.ptr = nullptr;
-                return this;
-            }
-            T & operator * (){
-                if(!ptr) throw 1;
-                return *ptr;
-             }
-            T & operator -> (){
-                if(!ptr) throw 1;
-                return *ptr;
-            }
-            T get_val(){
-                if(!ptr) throw 1;
-                return *ptr;
-            }
-        private:
-            T * ptr;
-            u_ptr(u_ptr<T>&){}
+struct u_ptr{
+    explicit u_ptr(T& a): ptr(new T){
+        *ptr = a;
+    }
+    ~u_ptr(){
+        if(!ptr) return;
+        delete ptr;
+        ptr = nullptr;
+    }
+    u_ptr<T>& operator = (u_ptr<T> & a){
+        if(!a.ptr) throw 1;
+        if(this == &a) return *this;
+        delete ptr;
+        ptr = a.ptr;
+        a.ptr = nullptr;
+        return *this;
+    }
+    T & operator * (){
+        if(!ptr) throw 1;
+        return *ptr;
+    }
+    T & operator -> (){
+        if(!ptr) throw 1;
+        return *ptr;
+    }
+    T get_val(){
+        if(!ptr) throw 1;
+        return *ptr;
+    }
+private:
+    T * ptr;
+    u_ptr(u_ptr<T>&){}
 
-        };
+};
+
+
+int main() {
+    int b = 10, c = 5;
+    u_ptr<int> a(b);
+    u_ptr<int> a1(c);
+    cout << a.operator*() << endl;
+    cout << a1.operator*()<<endl;
+    a = a1;
+    cout << a.operator*() << endl;
+
+}
