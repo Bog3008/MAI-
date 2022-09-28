48af71be310a61b23a87e9eae033a9651b098712
Bog3008 <106614305+Bog3008@users.noreply.github.com>
2022-09-28 11:57:00 +0300

    Create HW3.cpp

---
+++ Toporkov Bogdan 221/HW3.cpp
@@ -0,0 +1,33 @@
+template <class T>
+        struct u_ptr{
+            explicit u_ptr(T& a): ptr(new T){
+                *ptr = a;
+            }
+            ~u_ptr(){
+                if(!ptr) return;
+                delete ptr;
+            }
+            u_ptr<T>& operator = (u_ptr<T>a){
+                if(!a.ptr) throw 1;
+                if(this == &a) return this;
+                ptr = a.ptr;
+                a.ptr = nullptr;
+                return *this;
+            }
+            T & operator * (){
+                if(!ptr) throw 1;
+                return *ptr;
+             }
+            T & operator -> (){
+                if(!ptr) throw 1;
+                return *ptr;
+            }
+            T get_val(){
+                if(!ptr) throw 1;
+                return *ptr;
+            }
+        private:
+            T * ptr;
+            u_ptr(u_ptr<T>&){}
+
+        };
