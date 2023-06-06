b9aa7c45427ecee720fc22120cdc4180fc506945
Bog3008 <106614305+Bog3008@users.noreply.github.com>
2023-06-06 08:00:06 +0300

    Update task_3_DynamicProg.py

--- MethOpt_4sem/task_3_DynamicProg.py
+++ MethOpt_4sem/task_3_DynamicProg.py
@@ -1,18 +1,14 @@
-
 class MyItems():
     def __init__(self, name, weight, price):
         self._name = name
         self._weight = weight
         self._price = price
-
     @property
     def name(self):
         return self._name
-
     @property
     def weight(self):
         return self._weight
-
     @property
     def price(self):
         return self._price
@@ -24,7 +20,6 @@ def viewTable(table, index, capacity):
         print(str(i).ljust(sep_len), end='')
     print()
     print('-' * (capacity + 1)*sep_len)
-
     for name, row in zip(index, table):
         print(name.ljust(sep_len), end='|')
         for i in row:
@@ -33,10 +28,8 @@ def viewTable(table, index, capacity):
 def knapsack_max_value(itemsList, capacity):
     weights = [i.weight for i in itemsList]
     values = [i.price for i in itemsList]
-
     n = len(weights)
     dp = [[0] * (capacity + 1) for _ in range(n + 1)]
-
     for i in range(1, n + 1):
         for w in range(1, capacity + 1):
             if weights[i - 1] <= w:
@@ -44,14 +37,21 @@ def knapsack_max_value(itemsList, capacity):
             else:
                 dp[i][w] = dp[i - 1][w]
     index_names = ['empty'] + [i.name for i in itemsList]
+    selected_items = []
+    i, j = n, capacity
+    while i > 0 and j > 0:
+        if dp[i][j] != dp[i - 1][j]:
+            selected_items.append(index_names[i])
+            j -= weights[i - 1]
+        i -= 1
     viewTable(dp, index_names,capacity)
+    print('selected items is:', selected_items)
     return dp[n][capacity]
 
-
 myCapacity = 4
-itemsList = [MyItems('mag', weight=4, price=3000),
-             MyItems('notebook', weight=3, price=2000),
-             MyItems('guitar', weight=1, price=1500),]
+itemsList = [MyItems('stapler', weight=4, price=3000),
+             MyItems('pencil', weight=3, price=2000),
+             MyItems('note', weight=1, price=1500),]
 
 max_value = knapsack_max_value(itemsList,  myCapacity)
 print("Максимальная стоимость:", max_value)
